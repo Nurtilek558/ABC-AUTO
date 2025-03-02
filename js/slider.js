62b34e6477e0a3b759f3fbc37685453cede926e8
Nurtilek558 <navatovnurtilek105@gmail.com>
2025-03-02 11:41:49 +0600

    prototyp

---
+++ js/slider.js
@@ -0,0 +1,19 @@
+const swiper = new Swiper(".mySwiper", {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+    },
+});
